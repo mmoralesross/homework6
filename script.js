e8e15c4bc98b0bd42e26f3f3fde58b207df53a9f
“mmoralesross” <mmoralesross@gmail.com>
2020-11-07 16:56:13 -0500

    adding a class UI

---
+++ script.js
@@ -0,0 +1,16 @@
+class Fetch {
+    asyn getCurrent(input) {
+        const myKey = "40a0b759fb3ba5148a95bd0448b9693b";
+//make request to URL
+
+const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}`
+  );
+
+  const data = await response.json();
+
+  console.log(data);
+
+  return data;
+}
+}
